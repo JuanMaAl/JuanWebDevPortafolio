0cc4af20b6afcb556252ebdf1dd904711218be3d
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-22 07:58:37 +0200

    corrección ortográfica

--- assets/js/enlaces-portafolio.js
+++ assets/js/enlaces-portafolio.js
@@ -1,3 +1,19 @@
 (function() {
+    
+    const boton1 = document.querySelector("#boton1");
+    const boton2 = document.querySelector("#boton2");
+    const boton3 = document.querySelector("#boton3");
+
+    boton1.addEventListener('click', () => {
+        open('Braindead1800/Braindead1800.html');
     })
+
+    boton2.addEventListener('click', () => {
+        open('piano-angular/index.html');
+    })
+
+    boton3.addEventListener('click', () => {
+        open('404-not-found-master/404-notFound.html');
+    })
+
 })()
